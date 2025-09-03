83b9bb7e7ce480f4fc647d7254f78ee4b762a9f4
Catiel <Oliver.alandiaa@gmail.com>
2025-09-03 16:54:08 -0400

    Refactor simulation panels to use JTable for displaying results

--- src/SimulacionLanzamientos.java
+++ src/SimulacionLanzamientos.java
@@ -7,80 +7,85 @@ public class SimulacionLanzamientos {
         SwingUtilities.invokeLater(SimulacionLanzamientos::mostrarVentana);
     }
 
-    private static void mostrarVentana() {
-        JFrame frame = new JFrame("Simulación de Lanzamientos");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        JTabbedPane tabs = new JTabbedPane();
-
-        tabs.addTab("Moneda", new JScrollPane(simularMonedaPanel()));
-        tabs.addTab("Dado", new JScrollPane(simularDadoPanel()));
-        tabs.addTab("Dos Dados", new JScrollPane(simularDosDadosPanel()));
-
-        frame.add(tabs);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-
-    private static JTextArea simularMonedaPanel() {
+    private static JTable simularMonedaTabla() {
         int[] corridas = {10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000};
-        JTextArea area = new JTextArea();
-        area.setFont(new Font("Monospaced", Font.PLAIN, 14));
-        area.append("Corridas | Cara  | Cruz\n");
-        area.append("--------------------------\n");
-        for (int n : corridas) {
+        String[] columnas = {"Corridas", "Cara", "Cruz"};
+        Object[][] datos = new Object[corridas.length][3];
+        for (int i = 0; i < corridas.length; i++) {
+            int n = corridas[i];
             Random rand = new Random();
             int cara = 0, cruz = 0;
-            for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (rand.nextBoolean()) cara++;
                 else cruz++;
             }
-            area.append(String.format("%7d | %5d | %5d\n", n, cara, cruz));
+            datos[i][0] = n;
+            datos[i][1] = cara;
+            datos[i][2] = cruz;
         }
-        area.setEditable(false);
-        return area;
+        JTable tabla = new JTable(datos, columnas);
+        tabla.setEnabled(false);
+        return tabla;
     }
 
-    private static JTextArea simularDadoPanel() {
+    private static JTable simularDadoTabla() {
         int[] corridas = {10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000};
-        JTextArea area = new JTextArea();
-        area.setFont(new Font("Monospaced", Font.PLAIN, 14));
-        area.append("Corridas |  1   2   3   4   5   6\n");
-        area.append("-----------------------------------\n");
-        for (int n : corridas) {
+        String[] columnas = {"Corridas", "1", "2", "3", "4", "5", "6"};
+        Object[][] datos = new Object[corridas.length][7];
+        for (int i = 0; i < corridas.length; i++) {
+            int n = corridas[i];
             Random rand = new Random();
             int[] resultados = new int[6];
-            for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 resultados[rand.nextInt(6)]++;
             }
-            area.append(String.format("%7d | %3d %3d %3d %3d %3d %3d\n", n, resultados[0], resultados[1], resultados[2], resultados[3], resultados[4], resultados[5]));
+            datos[i][0] = n;
+            for (int k = 0; k < 6; k++) {
+                datos[i][k + 1] = resultados[k];
+            }
         }
-        area.setEditable(false);
-        return area;
+        JTable tabla = new JTable(datos, columnas);
+        tabla.setEnabled(false);
+        return tabla;
     }
 
-    private static JTextArea simularDosDadosPanel() {
+    private static JTable simularDosDadosTabla() {
         int[] corridas = {10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000};
-        JTextArea area = new JTextArea();
-        area.setFont(new Font("Monospaced", Font.PLAIN, 14));
-        area.append("Corridas |  2   3   4   5   6   7   8   9  10  11  12\n");
-        area.append("--------------------------------------------------------\n");
-        for (int n : corridas) {
+        String[] columnas = new String[12];
+        columnas[0] = "Corridas";
+        for (int i = 2; i <= 12; i++) {
+            columnas[i - 1] = String.valueOf(i);
+        }
+        Object[][] datos = new Object[corridas.length][12];
+        for (int i = 0; i < corridas.length; i++) {
+            int n = corridas[i];
             Random rand = new Random();
             int[] resultados = new int[11];
-            for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 int dado1 = rand.nextInt(6) + 1;
                 int dado2 = rand.nextInt(6) + 1;
                 resultados[dado1 + dado2 - 2]++;
             }
-            area.append(String.format("%7d | %3d %3d %3d %3d %3d %3d %3d %3d %3d %3d %3d\n",
-                n,
-                resultados[0], resultados[1], resultados[2], resultados[3], resultados[4], resultados[5],
-                resultados[6], resultados[7], resultados[8], resultados[9], resultados[10]
-            ));
+            datos[i][0] = n;
+            for (int k = 0; k < 11; k++) {
+                datos[i][k + 1] = resultados[k];
+            }
         }
-        area.setEditable(false);
-        return area;
+        JTable tabla = new JTable(datos, columnas);
+        tabla.setEnabled(false);
+        return tabla;
+    }
+
+    private static void mostrarVentana() {
+        JFrame frame = new JFrame("Simulación de Lanzamientos");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900, 600);
+        JTabbedPane tabs = new JTabbedPane();
+        tabs.addTab("Moneda", new JScrollPane(simularMonedaTabla()));
+        tabs.addTab("Dado", new JScrollPane(simularDadoTabla()));
+        tabs.addTab("Dos Dados", new JScrollPane(simularDosDadosTabla()));
+        frame.add(tabs);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
-
